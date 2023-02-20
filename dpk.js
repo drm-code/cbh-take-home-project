fb8ce7313989b7cf89494125bd8cc3d902af0554
Yonathan Pineda <yonathan.pineda@gmail.com>
2023-02-20 17:44:39 -0300

    Refactor

--- dpk.js
+++ dpk.js
@@ -3,26 +3,25 @@ const crypto = require("crypto");
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+  let candidate = TRIVIAL_PARTITION_KEY;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  if (!event) {
+    return candidate;
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
+  if (event.partitionKey) {
+    const isStringType = typeof event.partitionKey === "string";
+    candidate = isStringType
+      ? event.partitionKey
+      : JSON.stringify(event.partitionKey);
   } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+    const data = JSON.stringify(event);
+    candidate = crypto.createHash("sha3-512").update(data).digest("hex");
   }
+
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
     candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
   }
+
   return candidate;
 };
--- dpk.test.js
+++ dpk.test.js
@@ -5,4 +5,24 @@ describe("deterministicPartitionKey", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
+
+  describe("event.partitionKey", () => {
+    it("When it is string", () => {
+      const trivialKey = deterministicPartitionKey({
+        partitionKey: "YONATHAN",
+      });
+      expect(trivialKey).toBe("YONATHAN");
+    });
+    it("When it is not string", () => {
+      const trivialKey = deterministicPartitionKey({ partitionKey: 12345 });
+      expect(trivialKey).toBe("12345");
+    });
+    it("When 'event.partitionKey.length' > MAX_PARTITION_KEY_LENGTH", () => {
+      const trivialKey = deterministicPartitionKey({
+        partitionKey:
+          "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890",
+      });
+      expect(trivialKey.length).toBe(128);
+    });
+  });
 });
